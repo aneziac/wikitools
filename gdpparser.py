07fc892cef952dda49cefb0e28190640b22dce19
aneziac <nateannau@gmail.com>
2020-12-28 12:55:19 -0800

    many improvements and functional refactor

--- gdpparser.py
+++ gdpparser.py
@@ -2,57 +2,125 @@ import pandas as pd
 from weo import download
 from weo import WEO
 
-year = '2020'
-quarter = 2
 
-# calculate later
-col = 4
+def get_state_data(quarter, year, download_new=False):
 
-# create csv
-states = pd.read_excel('data/gdp/BEA_2019-2020.xlsx', sheet_name='Table 3', engine='openpyxl')
-states = states.iloc[5:64, [0, col]]
-states.columns = ['State/Country', '2019 GDP']
-states['State/Country'] = states['State/Country'].str.strip()
-states = states[~states['State/Country'].isin(['New England', 'Mideast', 'Great Lakes', 'Plains', 'Southeast', 'Southwest', 'Rocky Mountain', 'Far West'])]
-states = states.replace('Georgia', 'Georgia (U.S. state)|name=Georgia')
-states['2019 GDP'] = states['2019 GDP'].astype(float)
-states['State?'] = True
+    # read sheet
+    states = pd.read_excel('data/gdp/BEA_2019-2020.xlsx', sheet_name='Table 3')
 
-# download('2019-Oct', path='weo.csv', overwrite=True)
-# add automatic bea getter
+    # get data from specified quarter and year
+    # calculate later
+    col = 4
 
-w = WEO('weo.csv')
-countries = w.df
-countries = countries[countries['Subject Descriptor'] == 'Gross domestic product, current prices']
-countries = countries[countries['Units'] == 'U.S. dollars']
-countries = countries[['Country', '2019']]
-countries.columns = ['State/Country', '2019 GDP']
-countries['2019 GDP'] = countries['2019 GDP'].str.replace(',','')
-countries.loc[countries['State/Country'] == 'Syria', '2019 GDP'] = '60.043'
-countries['2019 GDP'] = countries['2019 GDP'].astype(float)
-countries['2019 GDP'] *= 1000
-countries['State?'] = False
+    states = states.iloc[5:64, [0, col]]
 
-data = pd.concat([states, countries])
-data.sort_values(by=['2019 GDP'], inplace=True, ascending=False)
-data = data.reset_index(drop=True)
-data = data.fillna(0.0)
+    # rename columns
+    states.columns = ['Country', 'GDP']
 
-data['Notes'] = ''
-data.loc[data['State/Country'] == 'China', 'Notes'] = 'Figures exclude Taiwan and special administrative regions of Hong Kong and Macau.'
-data.loc[data['State/Country'] == 'Syria', 'Notes'] = "Data for Syria's GDP is from the 2011 WEO Database, the latest available from the IMF."
-data.loc[data['State/Country'] == 'Russia', 'Notes'] = 'Figures exclude Republic of Crimea and Sevastopol.'
+    # clean up data 
+    states['Country'] = states['Country'].str.strip()
+    states = states[~states['Country'].isin(['New England', 'Mideast', 'Great Lakes', 'Plains', 'Southeast', 'Southwest', 'Rocky Mountain', 'Far West'])]
+    states = states.replace('Georgia', 'Georgia (U.S. state)|name=Georgia')
 
-out = open('out/countrystate.txt', 'w')
-out.write('{| class="wikitable sortable" style="text-align: right; margin-left: auto; margin-right: auto; border: none;"\n|+ National GDPs and U.S. State GDPs, ' + year)
-out.write('\n! Rank !! Country or U.S. state !! GDP (USD million)\n')
+    # convert gdp to float and create state column
+    states['GDP'] = states['GDP'].astype(float)
+    states['State?'] = True
 
-for row in range(len(data)):
-    color = ('style = "background: #D3D3D3; "' if data.iloc[row, 2] else '')
-    out.write('|-\n| ' + str(row + 1) + ' || align="left" ' + color + '| {{flag|' + data.iloc[row, 0] + '}}')
-    if data.iloc[row, 3] != '':
-        out.write('{{refn|' + data.iloc[row, 3] + '}}')
-    out.write(' || ' + f'{int(data.iloc[row, 1]):,}\n')
+    return states
 
-out.write('|}')
-out.close()
+def get_country_data(year, download_new=False):
+    if download_new:
+        download('2019-Oct', path='weo.csv', overwrite=True)
+
+    # read sheet
+    w = WEO('weo.csv')
+    countries = w.df
+
+    # grab the right data
+    countries = countries[countries['Subject Descriptor'] == 'Gross domestic product, current prices']
+    countries = countries[countries['Units'] == 'U.S. dollars']
+    countries = countries[['Country', year]]
+
+    # rename columns
+    countries.columns = ['Country', 'GDP']
+    
+    # clean up data
+    countries['GDP'] = countries['GDP'].str.replace(',','')
+    countries.loc[countries['Country'] == 'Syria', 'GDP'] = '60.043'
+    countries['GDP'] = countries['GDP'].astype(float)
+    countries['GDP'] *= 1000
+    countries['State?'] = False
+
+    # rename countries
+    replacements = {
+        'Korea': 'South Korea',
+        'Taiwan Province of China': 'Taiwan',
+        'Islamic Republic of Iran': 'Iran',
+        'Hong Kong SAR': 'Hong Kong',
+        'Slovak Republic': 'Slovakia',
+        'Macao SAR': 'Macau',
+        'Lao P.D.R.': 'Laos',
+        'Brunei Darussalam': 'Brunei',
+        'Kyrgyz Republic': 'Kyrgyzstan',
+        'Syria': 'Syria (2010)'
+    }
+    for item in replacements:
+        countries = countries.replace(item, replacements[item])
+
+    return countries
+
+def combine_data(sheet1, sheet2):
+    def add_note(country, note):
+        data.loc[data['Country'] == country, 'Notes'] = note 
+
+    # combine and sort data
+    data = pd.concat([sheet1, sheet2])
+    data.sort_values(by=['GDP'], inplace=True, ascending=False)
+    data = data.reset_index(drop=True)
+    data = data.fillna(0.0)
+
+    # add notes
+    data['Notes'] = ''
+    add_note('China', 'Figures exclude Taiwan and special administrative regions of Hong Kong and Macau.')
+    add_note('Syria (2010)', "Data for Syria's GDP is from the 2011 WEO Database, the latest available from the IMF.")
+    add_note('Russia', 'Figures exclude Republic of Crimea and Sevastopol.')
+
+    return data
+
+def write_wikitable(year, data):
+
+    # open file to write
+    out = open('out/countrystate.txt', 'w')
+
+    # create sortable centered wikitable with titles
+    out.write('{| class="wikitable sortable" style="text-align: right; margin-left: auto; margin-right: auto; border: none;"')
+    out.write('\n|+ National GDPs and U.S. State GDPs, ' + year)
+    out.write('\n! Rank !! Country or U.S. state !! GDP (USD million)\n')
+
+    # read data, add to file, and close file
+    for row in range(len(data)):
+        flag = '{{flag|' + data.iloc[row, 0] + '}}'
+
+        # bold US states
+        if data.iloc[row, 2]:
+            flag = "'''" + flag + "'''"
+
+        out.write('|-\n| ' + str(row + 1) + ' || align="left" | ' + flag)
+        if data.iloc[row, 3] != '':
+            out.write('{{refn|' + data.iloc[row, 3] + '}}')
+        out.write(' || ' + f'{int(data.iloc[row, 1]):,}\n')
+
+    out.write('|}')
+    out.close()
+
+def main():
+    year = '2019'
+    quarter = '2'
+
+    states = get_state_data(quarter, year)
+    countries = get_country_data(year)
+    data = combine_data(states, countries)
+    write_wikitable(year, data)
+
+
+main()
